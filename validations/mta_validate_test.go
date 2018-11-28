29b6771bc0f3c1961688b819b109c4992a5a38de
tals <shimon.tal@sap.com>
2018-11-28 21:17:44 +0200

    [fix] - modify fn name

--- validations/mta_validate_test.go
+++ validations/mta_validate_test.go
@@ -91,7 +91,7 @@ var _ = Describe("MTA tests", func() {
 		)
 
 		var _ = DescribeTable("validateMtaYaml", func(projectRelPath string, validateSchema, validateProject, expectedSuccess bool) {
-			err := ValidateMtaYaml(getTestPath(projectRelPath), "mta.yaml", validateSchema, validateProject)
+			err := MtaYaml(getTestPath(projectRelPath), "mta.yaml", validateSchema, validateProject)
 			Ω(err == nil).Should(Equal(expectedSuccess))
 		},
 			Entry("invalid path to yaml - all", "ui5app1", true, true, false),
