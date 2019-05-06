965fe77505e4e6318c24bf59bc4fd89e23898610
Koren <raya.koren@sap.com>
2019-05-06 14:13:38 +0300

    Add tests for copyFile and deleteFile

--- mta/mta_services_test.go
+++ mta/mta_services_test.go
@@ -10,18 +10,20 @@ import (
 )
 
 var _ = Describe("MtaServices", func() {
+
+	schemaVersion := "1.1"
+	oMtaInput := MTA{
+		ID:            "test",
+		Version:       "1.2",
+		SchemaVersion: &schemaVersion,
+		Description:   "test mta creation",
+	}
+
 	AfterEach(func() {
 		os.RemoveAll(getTestPath("result"))
 	})
 	var _ = Describe("CreateMta", func() {
 		It("Sanity", func() {
-			schemaVersion := "1.1"
-			oMtaInput := MTA{
-				ID:            "test",
-				Version:       "1.2",
-				SchemaVersion: &schemaVersion,
-				Description:   "test mta creation",
-			}
 			jsonData, err := json.Marshal(oMtaInput)
 			Ω(err).Should(Succeed())
 			mtaPath := getTestPath("result", "temp.mta.yaml")
@@ -31,6 +33,34 @@ var _ = Describe("MtaServices", func() {
 			oMtaOutput, err := Unmarshal(yamlData)
 			Ω(err).Should(Succeed())
 			Ω(reflect.DeepEqual(oMtaInput, *oMtaOutput)).Should(BeTrue())
+		})
+	})
+
+	var _ = Describe("CopyFile", func() {
+		It("Copy file content", func() {
+			jsonData, err := json.Marshal(oMtaInput)
+			Ω(err).Should(Succeed())
+			sourceFilePath := getTestPath("result", "temp.mta.yaml")
+			targetFilePath := getTestPath("result", "temp2.mta.yaml")
+			Ω(CreateMta(sourceFilePath, string(jsonData))).Should(Succeed())
+			Ω(CopyFile(sourceFilePath, targetFilePath)).Should(Succeed())
+			yamlData, err := ioutil.ReadFile(targetFilePath)
+			Ω(err).Should(Succeed())
+			oOutput, err := Unmarshal(yamlData)
+			Ω(err).Should(Succeed())
+			Ω(reflect.DeepEqual(oMtaInput, *oOutput)).Should(BeTrue())
+		})
+	})
+
+	var _ = Describe("deleteFile", func() {
+		It("Delete file", func() {
+			jsonData, err := json.Marshal(oMtaInput)
+			Ω(err).Should(Succeed())
+			mtaPath := getTestPath("result", "temp.mta.yaml")
+			Ω(CreateMta(mtaPath, string(jsonData))).Should(Succeed())
+			Ω(mtaPath).Should(BeAnExistingFile())
+			Ω(DeleteFile(mtaPath)).Should(Succeed())
+			Ω(mtaPath).ShouldNot(BeAnExistingFile())
 		})
 	})
 })
