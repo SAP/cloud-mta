2d2e9c91abdb22934f212b6aeff8710d191e9dfa
allaVolkov <42611923+allaVolkov@users.noreply.github.com>
2019-07-23 11:34:25 +0300

    Validations BLI 445 (#100)
    * Resolve MTA properties of a module, given env-variables and/or .env file
    
    * Fix review comments
    
    * code moved to internal; tests created
    
    * fix lint
    
    * fix lint
    
    * fix messages
    
    * BLI 445 validations related to unsupported features
    
    * fix lint
    
    * fix lint
    
    * some constants defined
    
    * texts fixes after review with Paola
    
    * code review fixes
    
    * code review fix
    
    * review fixes

---
+++ validations/semantic_deployer_constraints.go
@@ -0,0 +1,76 @@
+package validate
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v3"
+
+	"github.com/SAP/cloud-mta/mta"
+)
+
+const (
+	missingConfigDocLink = "https://sap.github.io/cloud-mta-build-tool/migration/#features-that-are-handled-differently-in-the-cloud-mta-build-tool"
+	missingConfigsMsg    = `the "%s" module does not contain the mandatory "supported-platforms" and "build-result" build configurations; see %q`
+	missingConfigMsg     = `the "%s" module does not contain the mandatory "%s" build configuration; see %q`
+)
+
+func checkDeployerConstraints(mta *mta.MTA, mtaNode *yaml.Node, source string, strict bool) ([]YamlValidationIssue, []YamlValidationIssue) {
+	var issues []YamlValidationIssue
+
+	if mta.Parameters != nil && mta.Parameters[deployModeYamlField] != nil {
+		deployMode, ok := mta.Parameters[deployModeYamlField].(string)
+		// case !ok is handled by schema validations
+		if ok && deployMode == html5RepoDeployMode {
+			issues = append(issues, checkHTML5ModulesParams(mta, mtaNode)...)
+		}
+	}
+
+	return issues, nil
+}
+
+func checkHTML5ModulesParams(mta *mta.MTA, mtaNode *yaml.Node) []YamlValidationIssue {
+	var issues []YamlValidationIssue
+
+	modulesNode := getPropContent(mtaNode, modulesYamlField)
+
+	for i, module := range mta.Modules {
+		if module.Type == html5ModuleType {
+			issues = append(issues, checkHTML5ModuleParams(module, modulesNode[i])...)
+		}
+	}
+
+	return issues
+}
+
+func checkHTML5ModuleParams(module *mta.Module, moduleNode *yaml.Node) []YamlValidationIssue {
+
+	supportedPlatformsDefined := false
+	buildResultDefined := false
+	if module.BuildParams != nil {
+		supportedPlatformsDefined = module.BuildParams[supportedPlatformsYamlField] != nil
+		buildResultDefined = module.BuildParams[buildResultYamlField] != nil
+	}
+
+	if !supportedPlatformsDefined && !buildResultDefined {
+		return []YamlValidationIssue{
+			{
+				Msg:  fmt.Sprintf(missingConfigsMsg, module.Name, missingConfigDocLink),
+				Line: moduleNode.Line,
+			},
+		}
+	} else if !supportedPlatformsDefined {
+		return []YamlValidationIssue{
+			{
+				Msg:  fmt.Sprintf(missingConfigMsg, module.Name, supportedPlatformsYamlField, missingConfigDocLink),
+				Line: moduleNode.Line,
+			},
+		}
+	} else if !buildResultDefined {
+		return []YamlValidationIssue{
+			{
+				Msg:  fmt.Sprintf(missingConfigMsg, module.Name, buildResultYamlField, missingConfigDocLink),
+				Line: moduleNode.Line,
+			},
+		}
+	}
+	return nil
+}
---
+++ validations/semantic_deployer_constraints_test.go
@@ -0,0 +1,69 @@
+package validate
+
+import (
+	"fmt"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/SAP/cloud-mta/mta"
+)
+
+var _ = Describe("SemanticDeployerConstraints", func() {
+	It("Sanity", func() {
+		mtaContent := []byte(`
+ID: mtahtml5
+_schema-version: '2.1'
+version: 0.0.1
+
+parameters:
+  deploy-mode: html5-repo
+
+modules:
+ - name: ui5app1
+   type: html5
+      
+ - name: ui5app2
+   type: html5
+   build-parameters:
+     builder: grunt
+
+ - name: ui5app3
+   type: html5
+   build-parameters:
+     supported-platforms: []
+
+ - name: ui5app4
+   type: html5
+   build-parameters:
+     build-result: dist
+
+ - name: ui5app5
+   type: html5
+   build-parameters:
+     supported-platforms: []
+     build-result: dist
+
+ - name: ui5app6
+   type: html5
+   build-parameters:
+     supported-platforms: [cf, neo]
+     build-result: dist1
+`)
+		mta, _ := mta.Unmarshal(mtaContent)
+		node, _ := getMtaNode(mtaContent)
+		errors, warn := checkDeployerConstraints(mta, node, "", true)
+		Ω(len(errors)).Should(Equal(4))
+		Ω(len(warn)).Should(Equal(0))
+		checkDeployerConstrError(errors[0], missingConfigsMsg, 10, "ui5app1")
+		checkDeployerConstrError(errors[1], missingConfigsMsg, 13, "ui5app2")
+		checkDeployerConstrError(errors[2], missingConfigMsg, 18, "ui5app3", buildResultYamlField)
+		checkDeployerConstrError(errors[3], missingConfigMsg, 23, "ui5app4", supportedPlatformsYamlField)
+	})
+})
+
+func checkDeployerConstrError(issue YamlValidationIssue, message string, line int, params ...interface{}) {
+	params = append(params, missingConfigDocLink)
+	Ω(issue.Msg).Should(Equal(fmt.Sprintf(message, params...)))
+	Ω(issue.Line).Should(Equal(line))
+
+}
