b26748847eb98108a566404e916562403ef59568
wangshen-sap <62389252+wangshen-sap@users.noreply.github.com>
2023-08-08 17:06:07 +0800

    xhttps://jira.tools.sap/browse/MBT-75 (#286)
    [research] Update MTA & MBT publish NPMJS process to remove binwrap dependency

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-var binwrap = require('binwrap');
-
-var packageInfo = require("./package.json");
-var version = packageInfo.version;
-var root = `https://github.com/SAP/cloud-mta/releases/download/v${version}/cloud-mta_${version}_`;
-
-module.exports = binwrap({
-    dirname: __dirname,
-    binaries: [
-        'mta'
-    ],
-    urls: {
-        'darwin-arm64': root + 'Darwin_arm64.tar.gz',
-        'darwin-x64': root + 'Darwin_amd64.tar.gz',
-        'linux-x64': root + 'Linux_amd64.tar.gz',
-        'win32-x64': root + 'Windows_amd64.tar.gz'
-    }
-});
+module.exports = {};
